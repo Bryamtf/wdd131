dacea60f74f313965152bf3998c2b20ca379c21d
Bryamtf <u20312070@utp.edu.pe>
2025-09-14 23:25:31 -0500

    update folder

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const lastModified = document.getElementById("lastModified");
+const dateLastModified = document.lastModified;
+
+lastModified.style.color='yellow'
+lastModified.textContent = `Last Modification: ${dateLastModified}`;
